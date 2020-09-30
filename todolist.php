0e73772451f6c97e479ab80c511126ac07f31e34
Artur Gawron <artur@gawron.me>
2020-09-30 19:26:44 +0200

    Init plugin

---
+++ todolist.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package todolist
+ */
+/*
+
+Plugin Name: todolist
+Plugin URI: http://gawron.me/plugin
+Description: ToDo list is a part of the recruitment process for a my dream job ❤️
+Version: 0.1.0
+Author: Artur Gawron
+Author URI: https://gawron.me
+License: GPLv2 or later
+Text Domain: todolist-plugin
+*/
