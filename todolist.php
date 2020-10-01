f2c313f1395dc9746c81ccdbf42ed58f6186b9f8
Artur Gawron <artur@gawron.me>
2020-10-01 13:57:33 +0200

    Add template for todo list and menu admin page

--- todolist.php
+++ todolist.php
@@ -40,7 +40,16 @@ if ( !class_exists( 'ToDoListPlugin' ) ) {
 	class ToDoListPlugin
 	{
 		function register() {
-			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+            add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+            add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+        }
+        
+        public function add_admin_pages() {
+            add_menu_page( 'To Do List', 'To Do List', 'manage_options', 'todolist_plugin', array( $this, 'todo_index' ), 'dashicons-list-view', 110 );
+        }
+
+        public function todo_index() {
+			require_once plugin_dir_path( __FILE__ ) . 'templates/todo_list.php';
 		}
 
 		protected function create_post_type() {
