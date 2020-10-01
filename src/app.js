b46121631f834b7e9d77cd15b90119e7ae8ad784
Artur Gawron <artur@gawron.me>
2020-10-02 01:11:34 +0200

    add dummy-task has been created

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
 import './sass/main.scss';
 
-window.addEventListener('load', function () {});
+window.addEventListener('load', function () {
+  const todoInput = document.querySelector('.form__input');
+  const todoButton = document.querySelector('.form__button');
+  const todoList = document.querySelector('.todo-container__list');
+
+  todoButton.addEventListener('click', addTodo);
+
+  function addTodo(event) {
+    event.preventDefault();
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const newTodo = document.createElement('li');
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+  }
+});
